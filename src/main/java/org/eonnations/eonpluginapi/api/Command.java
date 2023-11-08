77751c1b8d96bc2042ce5f938acdb5ad9ebc84ff
CodeSerenity <72942847+SerenityCodes@users.noreply.github.com>
2023-11-08 14:32:05 -0700

    Changed runtime retention policy

--- src/main/java/org/eonnations/eonpluginapi/api/Command.java
+++ src/main/java/org/eonnations/eonpluginapi/api/Command.java
@@ -1,5 +1,9 @@
 package org.eonnations.eonpluginapi.api;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
     String name();
     String usage();
